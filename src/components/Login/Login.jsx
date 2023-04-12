3543fed3fe8913c55557a8f585b765411774fd13
sajibsaifullah <dusajibphy@gmail.com>
2023-04-13 02:54:17 +0600

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../firebase/firebase.init";
+import { useState } from "react";
+
+const Login = () => {
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+    .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setUser(loggedUser);
+    })
+    .catch(error => {
+        console.log(error)
+    })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+        {
+            user ? 
+            <button onClick={handleSignOut}>Sign out</button>
+            :
+            <>
+                <button onClick={handleGoogleSignIn}>Google login</button>
+                <button onClick={handleGithubSignIn}>Github Login</button>
+            </>
+        }
+      
+      
+      {user && (
+        <div>
+          <h3>User: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwhpmQsr7zO_YMplLgZHvWVeooTvper1A",
+  authDomain: "simple-firebase-83cb5.firebaseapp.com",
+  projectId: "simple-firebase-83cb5",
+  storageBucket: "simple-firebase-83cb5.appspot.com",
+  messagingSenderId: "834848235792",
+  appId: "1:834848235792:web:db6be376549537c2ef4595"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
