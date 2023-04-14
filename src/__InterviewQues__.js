9629f52948019fac1792dafbc4ca29b1196dfa95
sajibsaifullah <dusajibphy@gmail.com>
2023-04-14 17:47:34 +0600

    InQ

---
+++ src/__InterviewQues__.js
@@ -0,0 +1,13 @@
+/**
+ * 1. What id Firebase?
+ * 2. Is firebase frontend or backend?
+ * 3. What are the features of firebase?
+ * 4. What are the tasks you can accomplish with firebase?
+ * 5. Differences between firebase and mongodb?
+ * 6. Have you ever used firebase database (real time database)?
+ * 7. Can you briefly explain the github authentication process with firebase?
+ * 8. Which method is used to sign-in the user in Firebase Email/Password authentication?
+ * 9. Authentication vs Authorization?
+ * 10. Can you tell me names of 9 authentication method?
+ * 11. Which authentication method have you ever used for your project purpose?
+*/
